20da1804156217988a541de0384e67c02dfa90fe
midestic <badmususman50@gmail.com>
2025-05-18 22:42:47 +0100

    bootstrap ignore

--- app/components/BootstrapClient.tsx
+++ app/components/BootstrapClient.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function BootstrapClient() {
   useEffect(() => {
-    //@ts-expect-error
+    //@ts-expect-error: Dynamic import for Bootstrap JS, no types needed
     import("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
